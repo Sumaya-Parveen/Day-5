d32955ae5c138f12a4b7dfc417c39643c2c1138d
Sumaya-Parveen <parv.n083@gmail.com>
2025-05-28 10:43:21 +0530

    entered code

--- Count_letters_in_strings.java
+++ Count_letters_in_strings.java
@@ -1,1 +1,22 @@
-class co
+class Count_letters_in_strings{
+    public static void main(String args[]){
+        String s="abcaadbca";   
+        String v=" ";
+        for(int i=0;i<s.length();i++){
+            int c=0;
+            char f=s.charAt(i);
+            if(v.indexOf(f)==-1){
+                for(int j=0;j<s.length();j++){
+                    char g=s.charAt(j);
+                    if(f==g){
+                    c++;
+                    }         
+                }
+                v=v+f;
+                System.out.println(s.charAt(i)+"->"+c);
+            }
+        }       
+    }
+}
+    
+
